779b66dd1736461155d0e54e3ce39e285b4c8867
sagid-veliev <78476654+sagid-veliev@users.noreply.github.com>
2021-03-19 18:15:25 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 function Matrix() {
       var n = document.getElementById("n").value;
-      var matrix = '<div class = "mat"><div class = "table">';
+      var matrix = " ";
       if (n > 0) {
          for (var i = 0; i < n; i++) {
-               matrix += "<table><tr>"
+               matrix += '<tr><table>'
                for (var j = 0; j < n; j++) {
 		   	matrix += '<td><input type="number" name = "value" placeholder = "0 or 1" min="0" max="1"></td>';
                }
-               matrix += "</tr></table>";       
+               matrix += '</tr></table>';       
          }
-         matrix += "</div><div>";
+         matrix += " ";
       }       // цикл для матрицы n x n
       else {
           alert("Введите значение > 0");   // если введено n < 0, выведет сообщение об этом
       } 
       document.getElementById('matrix').innerHTML = matrix; 
+}
+
+function correct() {
+      var n = document.getElementById("n").value;
+      var val = document.getElementsByName("value");
+      var schet = 0;
+      var arr = [];
+      for (var i = 0; i < n; i++){
+           arr[i] = [];
+           for (var j = 0; j < n; j++){
+        	  arr[i][j] = val[schet].value;
+		  schet++;
+	   }	
+      }
+      for (var i = 0; i < n; i++){   
+           for (var j = 0; j < n; j++){
+                 if(arr[i][j] > 1 || arr[i][j] < 0 ){
+                       return alert("ДОПУСТИМЫЕ ЗНАЧЕНИЯ ТОЛЬКО 0 и 1. ИСПРАВЬТЕ!!!");
+                 }
+	   }	 
+      }
+      return true;  
 }
 
 function graph() {
@@ -98,10 +120,39 @@ function symmetry() {
 	document.getElementById('symmetry').innerHTML = result;	
 }  
 
-function result() {
-     graph();
-     Matrix();
-     reflexivity();
-     antireflex();
+function transitivity(){
+       var n = document.getElementById("n").value;
+       var val = document.getElementsByName("value"); 
+       var schet = 0;
+       var result = 'Отношение является ТРАНЗИТИВНЫМ';
+	var arr = [];
+	for (var i = 0; i < n; i++){
+	      arr[i] = [];
+	      for (var j = 0; j < n; j++){
+		    arr[i][j] = val[schet].value;
+		    schet++;
+	      }	
+	}
+        for (var i = 0; i < n; i++){
+	      for (var j = 0; j < n; j++){
+                  if(arr[i][j] == 1) {
+                        for (var z = 0; z < n; z++) {
+                              if (arr[j][z] == !arr[i][z]) {
+                                   result = "Отношение НЕ является ТРАНЗИТИВНЫМ";
+                              }
+                        }
+                  }
+              }
+         }
+         document.getElementById('transitivity').innerHTML = result;
 }
 
+
+function result() {
+     correct();
+     graph();
+     reflexivity();
+     antireflex();
+     symmetry();
+     transitivity();
+}
